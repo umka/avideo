d8b0b15bf14267017277b0612884960792959c75
umka_kras <aleksandr@kozhukhovskiy.ru>
2021-06-02 16:59:23 +0700

    setup docker-compose

--- avideo/server/app.py
+++ avideo/server/app.py
@@ -4,14 +4,13 @@ from aiohttp import web
 import pdb
 
 
-
-
 async def get_food_image(request):
     food_name = request.match_info.get('food', "pizza")
     url=f'https://foodish-api.herokuapp.com/api/images/{food_name}'
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as request:
             text = await request.text()
+            #pdb.set_trace()
             print(request.status)
             print(text)
             return web.Response(text=text)
@@ -20,4 +19,4 @@ async def get_food_image(request):
 if __name__ == '__main__':
     app = web.Application()
     app.add_routes([web.get('/{food}', get_food_image)])
-    web.run_app(app, port=8088)
+    web.run_app(app, port=8081)
